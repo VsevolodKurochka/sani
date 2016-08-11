9fe97fce5dcca57c972ef34f2d5e7f22427b369d
VsevolodKurochka <dramdoobas123456@gmail.com>
2016-08-11 05:24:16 +0300

    Категория поправлено, левая колонка + чекбоксы

--- js/scripts.js
+++ js/scripts.js
@@ -166,4 +166,42 @@ $(document).ready(function(){
 		$(".show-sort-block-list").click(function(){
 			$(this).parent().next().slideToggle("slow");
 		});
+		$("#slider-price").slider({
+			min: 0,
+			max: 1000,
+			values: [0,1000],
+			range: true,
+			stop: function(event, ui) {
+        $("input#minCost").val($("#slider-price").slider("values",0));
+        $("input#maxCost").val($("#slider-price").slider("values",1));
+    },
+    slide: function(event, ui){
+        $("input#minCost").val($("#slider-price").slider("values",0));
+        $("input#maxCost").val($("#slider-price").slider("values",1));
+    }
+		});
+		$("input#minCost").change(function(){
+			var value1=$("input#minCost").val();
+			var value2=$("input#maxCost").val();
+		  if(parseInt(value1) > parseInt(value2)){
+				value1 = value2;
+				$("input#minCost").val(value1);
+			}
+			$("#slider-price").slider("values",0,value1);	
+		});
+
+			
+		$("input#maxCost").change(function(){
+			var value1=$("input#minCost").val();
+			var value2=$("input#maxCost").val();
+			
+			if (value2 > 1000) { value2 = 1000; $("input#maxCost").val(1000)}
+
+			if(parseInt(value1) > parseInt(value2)){
+				value2 = value1;
+				$("input#maxCost").val(value2);
+			}
+			$("#slider-price").slider("values",1,value2);
+		});
+
 });	
